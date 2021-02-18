875bf3e2f2b8d0c881157993f00d10dcd71dd2f1
Nassim <nassim.haddad.pro@gmail.com>
2021-02-18 11:52:22 +0100

    Final version of the blog Node project : routes + controllers + views

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
 
 // express app
 const app = express();
@@ -6,32 +9,35 @@ const app = express();
 // register view engine
 app.set('view engine', 'ejs');
 
-// listen for requests
-app.listen(3000);
+// connect to mongodb
+const dbURI = 'mongodb+srv://NassHad:Zizou123Blogs@node.h3omc.mongodb.net/node-course?retryWrites=true&w=majority';
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result) => {
+        // listen for requests
+        app.listen(3000);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
-app.get('/', (req, res) => {
-  // res.send('<p>home page</p>');
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    ];
-    res.render('index', {title: 'Home', blogs});
-})
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true})) // Takes all the urls and pass it in objects
+app.use(morgan('dev')); // Logs
 
+// blog routes
+app.use('/blogs', blogRoutes);
+
+// other routes
 app.get('/about', (req, res) => {
     // res.send('<p>about</p>');
     res.render('about',{title: 'About'});
-})
+});
 
-app.get('/blogs/create', ((req, res) => {
-    res.render('create', {title: 'Create'});
+// redirects
+app.get('/', ((req, res) => {
+    res.redirect('/blogs')
 }))
-
-/*// redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-})*/
 
 // 404 page
 app.use((req, res) => {
